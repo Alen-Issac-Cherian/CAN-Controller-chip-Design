0b166e0786742894ee7c129320977c78094f5353
Tejas K E <thejas222@gmail.com>
2021-05-05 14:17:38 +0530

    updates

--- Testbenches/seq_8_test.v
+++ Testbenches/seq_8_test.v
@@ -3,25 +3,25 @@
 //
 module seq_8_test ;
 
- reg clk, rst_n, load, din;
+ reg clk, rst, load, din;
  wire dout ;
  //reg [3:0] coun ;
  integer i;
 
  localparam period = 10;
 
- seq_8 uut (clk, rst_n, load, din, dout) ;
+ seq_8 uut (clk, rst, load, din, dout) ;
 
  initial begin
- $dumpfile("../Dumpfiles/seq_8_test.vcd");
+ $dumpfile("seq_8_test.vcd");
  $dumpvars(0, seq_8_test);
 
- rst_n = 1 ;
+ rst = 1 ;
  clk = 0 ;
  load = 0 ;
  din = 0 ;
  #25 ;
-  rst_n = 0 ;
+  rst = 0 ;
   load = 1 ;
   for(i = 0 ; i < 4 ; i = i+1) begin
    #40 ;
@@ -39,6 +39,7 @@ module seq_8_test ;
  #40 ;
   din = 1;
   $finish;
+ #100; 
  end
 
 always
